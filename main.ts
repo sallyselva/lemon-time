15fde7ed1357f0b1a8bfe589e9db00ec12199b1b
sallyselva <166712783+sallyselva@users.noreply.github.com>
2025-01-16 12:59:04 +0530

    Update main.ts

--- main.ts
+++ main.ts
@@ -8,23 +8,23 @@ let projectile: Sprite = null
 let mySprite: Sprite = null
 scene.setBackgroundColor(10)
 mySprite = sprites.create(img`
-    4 4 4 . . 4 4 4 4 4 . . . . . . 
-    4 5 5 4 4 5 5 5 5 5 4 4 . . . . 
-    b 4 5 5 1 5 1 1 1 5 5 5 4 . . . 
-    . b 5 5 5 5 1 1 5 5 1 1 5 4 . . 
-    . b d 5 5 5 5 5 5 5 5 1 1 5 4 . 
-    b 4 5 5 5 5 5 5 5 5 5 5 1 5 4 . 
-    c d 5 5 5 5 5 5 5 5 5 5 5 5 5 4 
-    c d 4 5 5 5 5 5 5 5 5 5 5 1 5 4 
-    c 4 5 5 5 d 5 5 5 5 5 5 5 5 5 4 
-    c 4 d 5 4 5 d 5 5 5 5 5 5 5 5 4 
-    . c 4 5 5 5 5 d d d 5 5 5 5 5 b 
-    . c 4 d 5 4 5 d 4 4 d 5 5 5 4 c 
-    . . c 4 4 d 4 4 4 4 4 d d 5 d c 
-    . . . c 4 4 4 4 4 4 4 4 5 5 5 4 
-    . . . . c c b 4 4 4 b b 4 5 4 4 
-    . . . . . . c c c c c c b b 4 . 
-    `, SpriteKind.Player)
+    4 4 4 . . 4 4 4 4 4 . . . . . .
+    4 5 5 4 4 5 5 5 5 5 4 4 . . . .
+    b 4 5 5 1 5 1 1 1 5 5 5 4 . . .
+    . b 5 5 5 5 1 1 5 5 1 1 5 4 . .
+    . b d 5 5 5 5 5 5 5 5 1 1 5 4 .
+    b 4 5 5 5 5 5 5 5 5 5 5 1 5 4 .
+    c d 5 5 5 5 5 5 5 5 5 5 5 5 5 4
+    c d 4 5 5 5 5 5 5 5 5 5 5 1 5 4
+    c 4 5 5 5 d 5 5 5 5 5 5 5 5 5 4
+    c 4 d 5 4 5 d 5 5 5 5 5 5 5 5 4
+    . c 4 5 5 5 5 d d d 5 5 5 5 5 b
+    . c 4 d 5 4 5 d 4 4 d 5 5 5 4 c
+    . . c 4 4 d 4 4 4 4 4 d d 5 d c
+    . . . c 4 4 4 4 4 4 4 4 5 5 5 4
+    . . . . c c b 4 4 4 b b 4 5 4 4
+    . . . . . . c c c c c c b b 4 .
+`, SpriteKind.Player)
 controller.moveSprite(mySprite)
 mySprite.setStayInScreen(true)
 game.onUpdateInterval(1000, function () {
